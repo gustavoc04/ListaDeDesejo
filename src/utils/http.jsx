736fb7449bc37953ac71a8afcf6ca14c848be9ef
GUSTAVO DE CASTRO CASTILHO COSTA <gustavodecastrocastilhocosta@outlook.com>
2024-05-22 22:05:23 -0300

    todas as funções implementadas para usar os endpoints da api

--- src/utils/http.jsx
+++ src/utils/http.jsx
@@ -29,13 +29,63 @@ export const login = async (userEmail) => {
     }
 }
 
-export const getCategorias = async () => {
+export const getCategory = async () => {
     try {
         const response = await axios.get(`${apiUrl}/categorias`)
         return response.data
 
     } catch (error) {
         console.error("Erro ao buscar categorias:", error);
+        throw error
+    }
+}
+
+export const getProductsByCategory = async (categoriaId, email) => {
+    try {
+        const response = await axios.get(`${apiUrl}/produtos/${categoriaId}/${email}`)
+        return response.data
+    } catch (error) {
+        console.error("Erro ao buscar produtos da categoria:", error);
+        throw error
+    }
+}
+
+export const getProductsFromWishlist = async (email) => {
+    try {
+        const response = await axios.get(`${apiUrl}/listadesejo/${email}`)
+        return response.data
+    } catch (error) {
+        console.error("Erro ao buscar produtos da lista de desejos:", error);
+        throw error
+    }
+}
+
+export const getProductsIdsFromWishlist = async (email) => {
+    try {
+        const response = await axios.get(`${apiUrl}/listadesejo/${email}/produtoIds`)
+        return response.data
+    } catch (error) {
+        console.error("Erro ao buscar id dos produtos da lista de desejos:", error);
+        throw error
+    }
+}
+
+export const postProductOnWishlist = async (produtoId, email) => {
+    try {
+        const response = await axios.post(`${apiUrl}/listadesejo/${produtoId}/${email}`)
+        return response.data
+    } catch (error) {
+        console.error("Erro ao adicionar produto à lista de desejos:", error);
+        throw error
+    }
+}
+
+export const deleteProdutcFromWishlist = async (produtoId, email) => {
+    try {
+        const response = await axios.delete(`${apiUrl}/listadesejo/${produtoId}/${email}`)
+        return response.data
+    } catch (error) {
+        console.error("Erro ao remover produto da lista de desejo:", error)
         throw error
     }
 }
